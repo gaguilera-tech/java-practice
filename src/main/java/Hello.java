0ce808b3e96b42344812c06869a591227e6e84b2
Lupita Aguilera <aguilera_21@hotmail.com>
2021-06-15 22:38:39 -0500

    First version

---
+++ src/main/java/Hello.java
@@ -0,0 +1,9 @@
+import pojo.Carro;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        Carro carro = new Carro("Rojo", 2018, "VW");
+        System.out.println(carro);
+    }
+}
---
+++ src/main/java/pojo/Carro.java
@@ -0,0 +1,49 @@
+package pojo;
+
+public class Carro {
+    private String color;
+    private int year;
+    private String marca;
+
+    public Carro() {
+    }
+
+    public Carro(String color, int year, String marca) {
+        this.color = color;
+        this.year = year;
+        this.marca = marca;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    @Override
+    public String toString() {
+        return "Carro{" +
+                "color='" + color + '\'' +
+                ", year=" + year +
+                ", marca='" + marca + '\'' +
+                '}';
+    }
+}
